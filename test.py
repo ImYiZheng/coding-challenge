e1189bce7b6b62e32ab7af657611acc1551dd4ee
ImYiZheng <yizhengsama@gmail.com>
2016-04-02 00:35:01 -0700

    Added files via upload

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import sys
 from datetime import datetime
-from src.average_degree import *
+from average_degree import *
 
-get_avgdegree = AvgDegree(sys.path[0]+'\\tweet_input\\tweets.txt',sys.path[0]+'\\tweet_output\\output.txt')
+get_avgdegree = AvgDegree(sys.path[0]+'\\tweet_input\\tweets1.txt',sys.path[0]+'\\tweet_output\\output1.txt')
 start_time = datetime.now()
 count = get_avgdegree.output_avg_degree()
 print "Finished! There are " + str(count) + ' tweets processed '+ 'and the total running time is ' + str((datetime.now() - start_time).total_seconds())+'s'
