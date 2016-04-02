078a02d3b56e07fe5ebc134a331531d71d9b2670
ImYiZheng <yizhengsama@gmail.com>
2016-04-02 00:36:38 -0700

    Added files via upload

--- insight_testsuite/my-own-test-2-tweets-larger60s.py
+++ insight_testsuite/my-own-test-2-tweets-larger60s.py
@@ -1,6 +1,6 @@
 import sys
 from datetime import datetime
-from src.average_degree import *
+from average_degree import *
 
 get_avgdegree = AvgDegree(sys.path[0]+'\\tests\\my-own-test-2-tweets-larger60s\\tweet_input\\tweets.txt',sys.path[0]+'\\tests\\my-own-test-2-tweets-larger60s\\tweet_output\\output.txt')
 start_time = datetime.now()
--- insight_testsuite/test-2-tweets-all-distinct.py
+++ insight_testsuite/test-2-tweets-all-distinct.py
@@ -1,6 +1,6 @@
 import sys
 from datetime import datetime
-from src.average_degree import *
+from average_degree import *
 
 get_avgdegree = AvgDegree(sys.path[0]+'\\tests\\test-2-tweets-all-distinct\\tweet_input\\tweets.txt',sys.path[0]+'\\tests\\test-2-tweets-all-distinct\\tweet_output\\output.txt')
 start_time = datetime.now()
