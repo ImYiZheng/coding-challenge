f82d7f174c1619d8d5b24bf867b55933e6d4faef
ImYiZheng <yizhengsama@gmail.com>
2016-04-02 21:33:43 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from average_degree import *
 get_avgdegree = AvgDegree(sys.path[0]+'\\tweet_input\\tweets.txt',sys.path[0]+'\\tweet_output\\output.txt')
 start_time = datetime.now()
 count_total_tweets, count_valid_tweets = get_avgdegree.output_avg_degree()
-print "Finished! There are "
+print "Finished!"
 print "There are "+ str(count_total_tweets) + " tweets in the input file"
 print str(count_valid_tweets) + ' tweets are processed '
 print 'the total running time is ' + str((datetime.now() - start_time).total_seconds())+'s'
