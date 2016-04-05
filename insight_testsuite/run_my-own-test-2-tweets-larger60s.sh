d1bfac899f4ef27853f62641fbde06310ea53f4e
ImYiZheng <yizhengsama@gmail.com>
2016-04-05 12:24:46 -0700

    Added files via upload

---
+++ insight_testsuite/run_my-own-test-2-tweets-larger60s.sh
@@ -0,0 +1,6 @@
+
+# argv[0]: codes that process input file
+# argv[1]: directory of input file
+# argv[2]: directory of output file
+
+python ./testsh.py ./tests/my-own-test-2-tweets-larger60s/tweet_input/tweets.txt ./tests/my-own-test-2-tweets-larger60s/tweet_output/output.txt
---
+++ insight_testsuite/run_test-2-tweets-all-distinct.sh
@@ -0,0 +1,6 @@
+
+# argv[0]: codes that process input file
+# argv[1]: directory of input file
+# argv[2]: directory of output file
+
+python ./testsh.py ./tests/test-2-tweets-all-distinct/tweet_input/tweets.txt ./tests/test-2-tweets-all-distinct/tweet_output/output.txt
